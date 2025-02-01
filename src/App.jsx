6680fd887e10f1885f541f9653523d0646f1951b
Rashad Edwards <rashad3412@users.noreply.github.com>
2025-02-01 13:37:56 -0500

    fixed scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
+import ScrollToTop from "./components/scrollEffect.jsx";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="bg-lightGray min-h-screen">
         <NavBar />
         <Routes>
